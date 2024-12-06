bbbd07d07c35fbf1fc62d9f9f17513200042bb5d
tawfiqbhyry <tawfik.2419010@stemdakahlia.moe.edu.eg>
2024-12-06 20:44:44 +0200

    changes

--- assesemt/src/constants/index.ts
+++
@@ -1,6 +0,0 @@
-export const STATUS_COLORS = {
-    "In progress": "bg-blue-100 text-blue-800 border-blue-200",
-    "Not started": "bg-slate-100 text-slate-800 border-slate-200",
-    Completed: "bg-green-100 text-green-800 border-green-200",
-    Cancelled: "bg-red-100 text-red-800 border-red-200",
-};
